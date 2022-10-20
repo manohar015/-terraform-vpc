663c434acb22d7e8d2054a34bffa15f966e80681
mvpc-11232 <mvp2612@hotmail.com>
2022-10-20 07:59:19 +0530

    Auto Push Commit

---
+++ env-dev/dev-backend.tfvars
@@ -0,0 +1,3 @@
+bucket = "b50-tf-state-bucket"
+key    = "robot/dev/terrafom.tfstate"
+region = "us-east-1"
--- provider.tf
+++ provider.tf
@@ -3,9 +3,5 @@ provider "aws" {
 }
 
 terraform {
-  backend "s3" {
-    bucket = "b50-tf-state-bucket"
-    key    = "robot/env/terrafom.tfstate"
-    region = "us-east-1"
-   }
+  backend "s3" {}
 }
