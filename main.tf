0bbb9fb6d46f94b6362c15596b0c5921028a610b
mvpc-11232 <mvp2612@hotmail.com>
2022-10-21 07:06:17 +0530

    Auto Push Commit

--- main.tf
+++ main.tf
@@ -5,6 +5,7 @@ module "vpc" {
   PUBLIC_SUBNET_CIDR  = var.PUBLIC_SUBNET_CIDR
   PRIVATE_SUBNET_CIDR = var.PRIVATE_SUBNET_CIDR
   AZ                  = var.AZ
+  DEFAULT_VPC_ID      = var.DEFAULT_VPC_ID
 }
 
 # Always the source attribute in terraform module cannot be parameterized.
--- terraform.auto.tfvars
+++ terraform.auto.tfvars
@@ -1,1 +1,2 @@
 AZ = ["us-east-1a", "us-east-1b"]
+DEFAULT_VPC_ID = "vpc-0031cc952da0c7bfc"
