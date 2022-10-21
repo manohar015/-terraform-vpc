0d44badec160ec36635cd698ac640b1be7073099
mvpc-11232 <mvp2612@hotmail.com>
2022-10-21 06:20:39 +0530

    Auto Push Commit

--- main.tf
+++ main.tf
@@ -1,12 +1,13 @@
 module "vpc" {
-  source   = "./vendor/modules/vpc"
-  VPC_CIDR = var.VPC_CIDR
+  source              = "./vendor/modules/vpc"
+  VPC_CIDR            = var.VPC_CIDR
+  PUBLIC_SUBNET_CIDR  = var.PUBLIC_SUBNET_CIDR
+  PRIVATE_SUBNET_CIDR = var.PRIVATE_SUBNET_CIDR
+  ENV                 = var.ENV
+
+
 }
 
 # Always the source attribute in terraform module cannot be parameterized.
 
 # if it cannot be handled or paramterize the source, how are we going to tell, fetch from x branch and  when branches are dynamic
-
-variable "PUBLIC_SUBNET_CIDR" {}
-variable "PRIVATE_SUBNET_CIDR" {}
-variable "ENV" {}
