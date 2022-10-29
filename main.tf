441725daf5deca6fd5a4377557c1f98a3a650040
mvpc-11232 <mvp2612@hotmail.com>
2022-10-29 07:07:01 +0530

    Auto Push Commit

--- main.tf
+++ main.tf
@@ -1,14 +1,15 @@
 module "vpc" {
-  source              = "./vendor/modules/vpc"
-  VPC_CIDR            = var.VPC_CIDR
-  ENV                 = var.ENV
-  PUBLIC_SUBNET_CIDR  = var.PUBLIC_SUBNET_CIDR
-  PRIVATE_SUBNET_CIDR = var.PRIVATE_SUBNET_CIDR
-  AZ                  = var.AZ
-  DEFAULT_VPC_ID      = var.DEFAULT_VPC_ID
-  DEFAULT_VPC_CIDR    = var.DEFAULT_VPC_CIDR
-  DEFAULT_VPC_RT      = var.DEFAULT_VPC_RT
-  Z04602961I29SHWLCRCU3
+  source                = "./vendor/modules/vpc"
+  VPC_CIDR              = var.VPC_CIDR
+  ENV                   = var.ENV
+  PUBLIC_SUBNET_CIDR    = var.PUBLIC_SUBNET_CIDR
+  PRIVATE_SUBNET_CIDR   = var.PRIVATE_SUBNET_CIDR
+  AZ                    = var.AZ
+  DEFAULT_VPC_ID        = var.DEFAULT_VPC_ID
+  DEFAULT_VPC_CIDR      = var.DEFAULT_VPC_CIDR
+  DEFAULT_VPC_RT        = var.DEFAULT_VPC_RT
+  PRIVATE_HOSTEDZONE_ID = var.PRIVATE_HOSTEDZONE_ID
+  
 }
 
 # Always the source attribute in terraform module cannot be parameterized.
