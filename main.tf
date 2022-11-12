f7ff10bafda073d35ba94810422930fbd97281ab
Manohar <manohar015@gmail.com>
2022-11-12 21:43:41 +0530

    Added my Comments

--- main.tf
+++ main.tf
@@ -6,8 +6,7 @@ module "vpc" {
   PRIVATE_SUBNET_CIDR   = var.PRIVATE_SUBNET_CIDR
   AZ                    = var.AZ
   DEFAULT_VPC_ID        = var.DEFAULT_VPC_ID
-  DEFAULT_VPC_CIDR      = var.DEFAULT_VPC_CIDR
-  DEFAULT_VPC_RT        = var.DEFAULT_VPC_RT
+
 }
 
 
