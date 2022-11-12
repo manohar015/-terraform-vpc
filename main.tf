3f7adc37d92dc207c01b6e4b4aa1d4406e007d00
Manohar <manohar015@gmail.com>
2022-11-12 19:57:54 +0530

    Added my Comments

--- main.tf
+++ main.tf
@@ -2,6 +2,9 @@ module "vpc" {
   source                = "./vendor/modules/vpc"
   VPC_CIDR              = var.VPC_CIDR
   ENV                   = var.ENV
+  PUBLIC_SUBNET_CIDR    = var.PUBLIC_SUBNET_CIDR
+  PRIVATE_SUBNET_CIDR   = var.PRIVATE_SUBNET_CIDR
+  AZ                    = var.AZ
 }
 
 
